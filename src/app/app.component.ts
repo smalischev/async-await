3aa7c394659e01d5472d26d12cb057271767d3b3
smalischev <s.malischev@googlemail.com>
2018-12-03 00:55:01 +0100

    asynchronous code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'async-wait';
+
+  tick =  Date.now();
+  log = v => console.log(`${v} \n Elapsed: ${Date.now() - this.tick} ms`);
+
+  loop = async seconds => {
+      const until = new Date().getTime() + seconds * 1000;
+      let now = new Date().getTime();
+      while (now < until) {
+        now = new Date().getTime();
+        await this.wait(1);
+      }
+      return `loop: ${seconds}`;
+
+  }
+
+  wait = (ms) => {
+    return new Promise(r => setTimeout(r, ms));
+  }
+
+  ngOnInit() {
+    this.log('synchronous 1');
+    (async () => (async () => {
+      const a = this.loop(10).then( result => {this.log(result); return result; });
+      const b = this.loop(3).then( result => {this.log(result); return result; });
+      const result = await Promise.all([a, b]);
+      console.log(result + '');
+    })())();
+    this.log('synchronous 2');
+  }
 }
